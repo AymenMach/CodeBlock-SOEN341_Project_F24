4aafeb0f27d03fb67f443f0a8277e35931210bad
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-24 16:03:10 -0400

    Update Back-End Code in Node.js

--- Back-End Code in Node.js
+++ Back-End Code in Node.js
@@ -1,1 +1,17 @@
 // Back-End Code in Node.js
+
+const http = require ('http')
+
+// server creation
+const server = http.createServer((request, response) => {
+  console.log(request.url)
+
+  respone.end('Hello Node JS')
+})
+
+// server responds with hello node js
+server.listen(3000)
+
+// node index.js
+// visit port 3000
+// localhost 3000
