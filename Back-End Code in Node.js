be922a405e964594911f41a468a6450487f15cd7
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-24 18:32:15 -0400

    Update Back-End Code in Node.js

--- Back-End Code in Node.js
+++ Back-End Code in Node.js
@@ -4,10 +4,23 @@ const http = require ('http')
 
 // server creation
 const server = http.createServer((request, response) => {
-  console.log(request.url)
 
-  respone.end('Hello Node JS')
+  if (request.url === '/about') {
+    return response.end('About Page')
+  
+  } else if (request.url === '/contact') {
+    return response.end('Contact Page')
+  
+  } else if (request.url === '/') {
+    return response.end('Home Page')
+  
+  } else {
+    response.writeHead(404)
+
+    response.end('Page not Found')
+  }
 })
+                          
 
 // server responds with hello node js
 server.listen(3000)
