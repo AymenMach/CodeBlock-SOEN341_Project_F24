809ce7628e6fbd8ebd1093c7ccc299c34176a8e4
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-26 15:20:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
@@ -7,3 +9,18 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
+
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({username});
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password'});
+    }
+  }};
+} catch (error) {
+console.error('Logging In Error', error);
+  res.status(5000).json({ error: 'Error occured while logging in' });
+}
+});
+  
