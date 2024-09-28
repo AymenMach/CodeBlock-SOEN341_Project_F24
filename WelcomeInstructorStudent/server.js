1e92b5ffea0948ac63b613dde958379c6ac453ae
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 14:05:19 -0400

    Update server.js

--- WelcomeInstructorStudent/server.js
+++ WelcomeInstructorStudent/server.js
@@ -10,3 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
