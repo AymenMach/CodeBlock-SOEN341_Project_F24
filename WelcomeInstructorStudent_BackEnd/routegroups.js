ab35b0371d18e1adb1e8b88f200222eff9c479d2
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 15:42:08 -0400

    Create routegroups.js

---
+++ WelcomeInstructorStudent_BackEnd/routegroups.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const group = require('group');
+const router = express.Router();
+
+router.get('/groups', async (req, res) => {
+  try {
+    const groups = await Group.find().populate('students', 'username');
+    res.status(200).json(groups);
+  }
+  catch (error) {
+    res.status(500).json({ message: 'Error routing groups' });
+  }
+});
+
+module.exports = router;
