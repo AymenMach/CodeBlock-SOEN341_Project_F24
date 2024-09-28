64a5c8c025fc1bb480da353caacfff42744b602d
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 16:35:38 -0400

    Update group.js

--- WelcomeInstructorStudent_BackEnd/group.js
+++ WelcomeInstructorStudent_BackEnd/group.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const groupSchema = new mongoose.Schema ({
-  name: {type: String, required: true },
-  students: [{type: mongoose.Schema.Types.ObjectID, ref: 'User' }]
-}];
+const groupSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] 
+});
 
 module.exports = mongoose.model('Group', groupSchema);
