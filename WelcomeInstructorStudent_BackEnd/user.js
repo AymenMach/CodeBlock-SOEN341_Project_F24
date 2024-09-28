c49c0579d0ba1d69a3f81814447c0b3232273504
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 15:37:42 -0400

    Update user.js

--- WelcomeInstructorStudent_BackEnd/user.js
+++ WelcomeInstructorStudent_BackEnd/user.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, 
+  username: {type: String, required: true, unique: true };
+  password: {type: String, required: true },
+  role: {type: String, enum: ['student', 'instructor'], required: true },
+  group: {type: String }
+});
+
+module.exports = mongoose.model('user', userSchema);
