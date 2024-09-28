28699f4cda95f829f1fdb3e55c7f2348df18ea12
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 14:08:20 -0400

    Update server.js

--- WelcomeInstructorStudent/server.js
+++ WelcomeInstructorStudent/server.js
@@ -14,3 +14,6 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
+app.use('/api/users', userRoutes);
+app.use('/api/groups', groupRoutes);
+
