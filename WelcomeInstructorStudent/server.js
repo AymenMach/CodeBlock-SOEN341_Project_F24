df58b8214970bb22b1995fb20e3359eec8aee0dd
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 15:33:16 -0400

    Update server.js

--- WelcomeInstructorStudent/server.js
+++ WelcomeInstructorStudent/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const Group = require('.models/Group');
+const {parseCSVAndStoreData} = require('./utils/csvParser');
 
-const userRoutes = require('./routes/userRoutes');
-const groupRoutes = require('./routes/groupRoutes');
-
+require('dotenv').config();
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-app.use('/api/users', userRoutes);
-app.use('/api/groups', groupRoutes);
+const importCSV = async () => {
+  const fs = require('fs');
+  const csv = fs.readFileSync('./public/student.csv', 'utf8');
+  parseCSVAndStoreData(csv);
+};
+
+importCSV();
 
 const port = process.env.port || 3000;
 app.listen(3000, () => {
