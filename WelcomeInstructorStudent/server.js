aaaad3b59cea73a7be41b4482ad9775f4ec4810a
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 16:34:12 -0400

    Update server.js

--- WelcomeInstructorStudent/server.js
+++ WelcomeInstructorStudent/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const groupRoutes = require('./routes/routeGroups');
-const userRoutes = require('./routes/userRoutes');
+const groupRoutes = require('./routes/routegroups'); 
+const userRoutes = require('./routes/userRoutes'); 
+const { parseCSVAndStoreData } = require('./utils/csvParser'); 
 
 require('dotenv').config();
 const app = express();
@@ -11,16 +12,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
+const importCSV = async () => {
+  const csvFilePath = './public/student.csv'; 
+  parseCSVAndStoreData(csvFilePath);
+};
+
 importCSV();
 
-app.use('/api/users', userRoutes);
+// Routes
 app.use('/api/groups', groupRoutes); 
+app.use('/api/users', userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Server running on port 3000);
+  console.log(`Server running on port ${port}`);
 });
