5491bf2eed7ee2997d0f48c360a88af254308704
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-29 16:48:14 -0400

    Update server.js

--- Project_1/server.js
+++ Project_1/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth'); // Import auth routes
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const {parseCSVAndStoreData} = require('./utils/csvParser');
+
 const groupRoutes = require('./routes/routegroups'); 
 const userRoutes = require('./routes/userRoutes'); 
-const {parseCSVAndStoreData} = require('./utils/csvParser');
+const authRoutes = require('./routes/auth');
+
 
 require('dotenv').config();
 const app = express();
@@ -15,7 +17,7 @@ app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+  .catch(e => console.error(e));
 
 const importCSV = async () => {
   const csvFilePath = './public/student.csv'; 
@@ -30,6 +32,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/users', authRoutes);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(5000, () => {
+  console.log(`Server running on port 5000`);
 });
