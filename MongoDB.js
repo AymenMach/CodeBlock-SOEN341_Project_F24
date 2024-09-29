d1f5fdac5e9afe7c97201674eed8639814fa4472
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-09-28 20:49:48 -0400

    Update MongoDB.js

--- MongoDB.js
+++ MongoDB.js
@@ -1,10 +1,1 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost/login', {
-  userNewUrlParser: true, 
-  useUnifiedTopology: true, 
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.log('Failed to Connect to MongoDB', error));
-                      
