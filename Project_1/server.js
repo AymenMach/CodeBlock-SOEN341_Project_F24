af3f5f2ef90849851b231da902d72b3718fbf0af
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-27 19:24:22 -0400

    Update server.js

--- Project_1/server.js
+++ Project_1/server.js
@@ -1,37 +1,30 @@
+// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const {parseCSVAndStoreData} = require('./utils/csvParser');
-
-const groupRoutes = require('./routes/routegroups'); 
-const userRoutes = require('./routes/userRoutes'); 
+const { parseCSVAndStoreData } = require('./utils/csvParser');
+const groupRoutes = require('./routes/routegroups');
+const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/auth');
-
-
 require('dotenv').config();
-const app = express();
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(e => console.error(e));
-
-const importCSV = async () => {
-  const csvFilePath = './public/student.csv'; 
-  parseCSVAndStoreData(csvFilePath);
-};
-
-importCSV();
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    parseCSVAndStoreData('./data/student.csv'); // Import CSV on initial connection
+  })
+  .catch(e => console.error('MongoDB connection error:', e));
 
 // Routes
-app.use('/api/groups', groupRoutes); 
+app.use('/api/groups', groupRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/users', authRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
-  console.log(`Server running on port 5000`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
