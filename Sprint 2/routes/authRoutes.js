d80a5ed324cb0369a052359a7be1fb44f9374f52
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-27 18:47:02 -0400

    Update authRoutes.js

--- Sprint 2/routes/authRoutes.js
+++ Sprint 2/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+// Login route
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   
