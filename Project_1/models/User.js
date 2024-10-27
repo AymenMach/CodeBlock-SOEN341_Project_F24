a011030f51eed4c39a96add1163bd49648858ee5
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-27 19:24:48 -0400

    Update User.js

--- Project_1/models/User.js
+++ Project_1/models/User.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['student', 'instructor'], required: true }, // Role is required
-  group: { type: String } // Group is optional, for example, instructors might not belong to a group
+  role: { type: String, enum: ['student', 'instructor'], required: true },
+  group: { type: String } // Optional for students
+});
+
+// Pre-save hook for hashing password
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
