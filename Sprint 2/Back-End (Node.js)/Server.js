9176767c8950e96a89445500ebcc53afef65a367
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-26 22:51:57 -0400

    Update Server.js

--- Sprint 2/Back-End (Node.js)/Server.js
+++ Sprint 2/Back-End (Node.js)/Server.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/peerAssessment', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
