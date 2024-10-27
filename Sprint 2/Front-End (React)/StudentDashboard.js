be72eda363e89681591ffa2d819c82f2a1af2314
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-26 22:33:51 -0400

    Update StudentDashboard.js

--- Sprint 2/Front-End (React)/StudentDashboard.js
+++ Sprint 2/Front-End (React)/StudentDashboard.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import DisplayGroups from './DisplayGroups';
+import './StudentDashboard.css';
 
+const StudentDashboard = () => {
+  return (
+    <div className="student-dashboard">
+    <h1>Welcome to Your Dashboard</h1>
+    <p>Here are the groups</p>
+    <DisplayGroups />
+  </div>
+  );
+};
+
+export default StudentDashboard;
