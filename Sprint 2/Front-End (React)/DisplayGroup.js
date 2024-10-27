cf74a925f27f6cc681ccb0223a5522ce2c9d8e9e
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-26 22:18:23 -0400

    Update DisplayGroup.js

--- Sprint 2/Front-End (React)/DisplayGroup.js
+++ Sprint 2/Front-End (React)/DisplayGroup.js
@@ -1,1 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './DisplayGroups.css';
 
+const DisplayGroups = () => {
+  return (
+    <div className="display-groups">
+      <h2>Group List</h2>
+    </div>
+  );
+};
+
+export default DisplayGroups;
