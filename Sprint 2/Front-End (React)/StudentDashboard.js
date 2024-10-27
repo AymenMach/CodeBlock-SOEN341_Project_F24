b0bef2213583d8e6a40d6fa9f7661e653ccc36d4
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-26 22:45:10 -0400

    Update StudentDashboard.js

--- Sprint 2/Front-End (React)/StudentDashboard.js
+++ Sprint 2/Front-End (React)/StudentDashboard.js
@@ -5,10 +5,25 @@ import './StudentDashboard.css';
 const StudentDashboard = () => {
   return (
     <div className="student-dashboard">
-    <h1>Welcome to Your Dashboard</h1>
-    <p>Here are the groups</p>
-    <DisplayGroups />
-  </div>
+      <h1>Your Groups</h1>
+      <p>List of groups you are a part of:</p>
+      <div className="group-list">
+        <div className="group">
+          <h2>Group A</h2>
+          <ul>
+            <li>Alice</li>
+            <li>Bob</li>
+          </ul>
+        </div>
+        <div className="group">
+          <h2>Group B</h2>
+          <ul>
+            <li>Charlie</li>
+            <li>Dave</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
