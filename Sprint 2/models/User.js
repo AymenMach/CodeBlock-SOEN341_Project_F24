9ae7ad6f6311ea652e48596a1ca959402c52c99c
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-26 23:12:14 -0400

    Update User.js

--- Sprint 2/models/User.js
+++ Sprint 2/models/User.js
@@ -4,35 +4,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ['student', 'instructor'] }, // Restrict role to either 'student' or 'instructor'
+  role: { type: String, required: true, enum: ['student', 'instructor'] },
 });
 
 // Compile model from schema
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
-
-// Pre-set login credentials
-const initializeUsers = async () => {
-  try {
-    // Define initial users
-    const initialUsers = [
-      { username: 'Aymen', password: '40250403', role: 'instructor' },
-      { username: 'Nihal', password: '40250403', role: 'student' },
-    ];
-
-    // Check if users already exist
-    const existingUsers = await User.find({});
-    if (existingUsers.length === 0) {
-      await User.insertMany(initialUsers);
-      console.log('Pre-set users have been added.');
-    } else {
-      console.log('Users already initialized.');
-    }
-  } catch (error) {
-    console.error('Error initializing users:', error);
-  }
-};
-
-initializeUsers();
-
