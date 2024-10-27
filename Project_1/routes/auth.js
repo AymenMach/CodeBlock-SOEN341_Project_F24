aeb023f8d9087ed6590ababb920cb7bd5583c955
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-27 19:25:25 -0400

    Update auth.js

--- Project_1/routes/auth.js
+++ Project_1/routes/auth.js
@@ -1,25 +1,20 @@
+// auth.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Import User model
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
-// Login Route
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Find the user by username
     const user = await User.findOne({ username });
-    
-    // Check if the user exists and the password matches
-    if (user && user.password === password) {
-      // Successful login
-      res.status(200).json({ message: 'Login successful', user });
-    } else {
-      // Invalid credentials
-      res.status(401).json({ message: 'Invalid credentials' });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
+    res.status(200).json({ message: 'Login successful', role: user.role, username: user.username });
   } catch (error) {
-    console.error('Error during login:', error);
+    console.error('Login error:', error);
     res.status(500).json({ message: 'Server error' });
   }
 });
