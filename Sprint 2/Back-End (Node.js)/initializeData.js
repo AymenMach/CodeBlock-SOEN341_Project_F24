9e4abbed8f0e098b82d83763633dd21a7c83fb24
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-10-27 21:00:04 -0400

    Update initializeData.js

--- Sprint 2/Back-End (Node.js)/initializeData.js
+++ Sprint 2/Back-End (Node.js)/initializeData.js
@@ -1,35 +1,22 @@
 const fs = require('fs');
 const csvParser = require('csv-parser');
 const mongoose = require('mongoose');
-const user = require('./models/user');
+const User = require('./models/User');
 
 mongoose.connect('mongodb://localhost:27017/peerAssessment', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('MongoDB connection error:', error));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
-// Import CSV data into MongoDB
 const importCSV = async (filePath) => {
   const users = [];
   fs.createReadStream(filePath)
     .pipe(csvParser())
     .on('data', (row) => {
-      const user = {
-        username: row.username,
-        password: row.password,
-        name: row.name,
-        role: row.role,
-      };
-
-      if (row.role === 'student') {
-        user.studentID = generateStudentID().toString();
-      }
-
-      users.push(user);
-    })
-    .on('end', async () => {
+      users.push({ username: row.username, password: row.password, name: row.name, role: row.role,});
+    }) .on('end', async () => {
       try {
         await User.insertMany(users);
         console.log('CSV data successfully imported.');
