9c5c29b71d418e4ab8e1aa362d5ffce8cfa22ec5
ChristopherPuran <148171996+ChristopherPuran@users.noreply.github.com>
2024-10-31 13:45:05 -0400

    Modified to require participants.

--- Sprint 1/models/group.js
+++ Sprint 1/models/group.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+// Creates group collection in MongoDB
 const groupSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] 
+  name: { type: String, required: true, unique: true },
+  participants: [{ type: String }], // Added participants as an array of strings
 });
 
-module.exports = mongoose.model('Group', groupSchema);
+module.exports = mongoose.model('groups', groupSchema);
