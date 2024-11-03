e4e186bd33fc7f15c3f32129b5968b3a84cc1cba
ChristopherPuran <148171996+ChristopherPuran@users.noreply.github.com>
2024-11-02 23:44:16 -0400

    Updated server to handle registration

--- Sprint 3/server.js
+++ Sprint 3/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth'); // Import auth routes
+const authRoutes = require('./routes/auth'); /
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const groupRoutes = require('./routes/routegroups'); 
 const userRoutes = require('./routes/userRoutes'); 
-const {parseCSVAndStoreData} = require('./utils/csvParser');
+const { parseCSVAndStoreData } = require('./utils/csvParser');
 
 require('dotenv').config();
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+
+
+app.use('/api/users', userRoutes); 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
@@ -22,13 +26,11 @@ const importCSV = async () => {
   parseCSVAndStoreData(csvFilePath);
 };
 
-
 importCSV();
 
 // Routes
 app.use('/api/groups', groupRoutes); 
-app.use('/api/users', userRoutes);
-app.use('/api/users', authRoutes);
+app.use('/api/users', authRoutes); 
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
