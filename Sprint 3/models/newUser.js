c5a6488d3d4f68feb2e0af06a48df89a99997ec7
ChristopherPuran <148171996+ChristopherPuran@users.noreply.github.com>
2024-11-04 15:28:47 -0500

    newUser for registration now includes the studentId

--- Sprint 3/models/newUser.js
+++ Sprint 3/models/newUser.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
-// Create User collection in MongoDB
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   role: { type: String, default: 'student' },
-  belongsToGroup: { type: String } 
+  studentId: { type: Number, required: true }, 
+  belongsToGroup: { type: String }
 });
 
-
-// Ensure the model is only created once
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 module.exports = User;
