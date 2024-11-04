afbbf6e4ed816c2f8b8cfd7a1ba60e9d9177c537
ChristopherPuran <148171996+ChristopherPuran@users.noreply.github.com>
2024-11-04 15:28:07 -0500

    User model now includes studentId

--- Sprint 3/models/User.js
+++ Sprint 3/models/User.js
@@ -1,25 +1,14 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-
-// Create User collection in MongoDB
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: {type: String, required: true},
-  role: { type: String, enum: ['student', 'instructor'], required: true },
-  belongsToGroup: {type: String}
+  password: { type: String, required: true }, 
+  name: { type: String, required: true },
+  role: { type: String, default: 'student', required: true }, 
+  studentId: { type: Number, required: true }, 
 });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    //this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
-
-// Ensure the model is only created once
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 module.exports = User;
+
