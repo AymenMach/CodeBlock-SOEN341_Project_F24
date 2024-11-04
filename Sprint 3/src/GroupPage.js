57977a376218e5d9d8a9aa3ade12a59c1f84fba2
ChristopherPuran <148171996+ChristopherPuran@users.noreply.github.com>
2024-11-04 15:33:54 -0500

    No longer used

--- Sprint 3/src/GroupPage.js
+++ Sprint 3/src/GroupPage.js
@@ -1,3 +1,7 @@
+/*========================================
+              NOT IN USE
+==========================================*/
+
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
