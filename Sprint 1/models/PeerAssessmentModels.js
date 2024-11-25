c059f4cd7e28c068da0aa7f3eae8225c6eb3d857
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-11-25 13:26:21 -0500

    Delete Sprint 1 directory

--- Sprint 1/models/PeerAssessmentModels.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const peerAssessmentSchema = new mongoose.Schema({
-  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'groups', required: true },
-  ratings: { type: Map, of: Number },
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('PeerAssessment', peerAssessmentSchema);
