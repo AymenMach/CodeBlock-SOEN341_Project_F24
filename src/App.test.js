4dcc48b311b0362f6fc2d964f3406f411f328dff
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-11-26 18:50:35 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,13 +5,5 @@ import { UserProvider } from './UserContext';
 import App from './App';
 
 test('renders the Peer Assessment System header', () => {
-  render(
-    <UserProvider>
-      <Router> {/* Only a single Router is used */}
-        <App />
-      </Router>
-    </UserProvider>
-  );
-  const headerElement = screen.getByText(/PEER ASSESSMENT SYSTEM/i);
-  expect(headerElement).toBeInTheDocument();
+  render(<App />);
 });
