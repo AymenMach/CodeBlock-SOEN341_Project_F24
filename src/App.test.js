39859df2d24bd222598115011c5db734ba947bc6
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-11-27 12:27:04 -0500

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
-import { UserProvider } from './UserContext';
-import App from './App';
-
-test('renders the Peer Assessment System header', () => {
-  render(<App />);
-});
