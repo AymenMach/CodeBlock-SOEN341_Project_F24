5b873c21d374757bba5f26c36c5cbc0368390bfc
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-11-27 14:59:27 -0500

    Update and rename createGroup.test.js to CreateGroup.test.js

--- tests/CreateGroup.test.js
+++ tests/CreateGroup.test.js
@@ -1,5 +1,5 @@
 // Unit test CreateGroup
-const { CreateGroup } = require('../controllers/CreateGroup'); 
+const { createGroup } = require('../controllers/CreateGroup'); 
 const Group = require('../models/group');
 const User = require('../models/User'); 
 
