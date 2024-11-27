aeb777bf072b3cdbac8542512006690df5412385
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-11-26 19:07:52 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { UserProvider } from './UserContext';
 import App from './App';
 
 test('renders the Peer Assessment System header', () => {
-  render(
-    <UserProvider>
-      <Router> {/* Only a single Router is used */}
-        <App />
-      </Router>
-    </UserProvider>
-  );
-  const headerElement = screen.getByText(/PEER ASSESSMENT SYSTEM/i);
-  expect(headerElement).toBeInTheDocument();
   render(<App />);
 });
