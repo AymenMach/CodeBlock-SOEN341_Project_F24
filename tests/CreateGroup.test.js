39c7623923c47d9fe99038555afd4e7e815cc0f1
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2024-11-27 15:06:34 -0500

    Update CreateGroup.test.js

--- tests/CreateGroup.test.js
+++ tests/CreateGroup.test.js
@@ -1,4 +1,4 @@
-// Unit test CreateGroup
+// Unit test controllers/CreateGroup.js
 const { createGroup } = require('../controllers/CreateGroup'); 
 const Group = require('../models/group');
 const User = require('../models/User'); 
